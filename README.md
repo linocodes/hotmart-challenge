1986d76e7b35fec9c9525061c4773cf79eff546e
Marcelo Lino <marcelo.lino.santos@gmail.com>
2020-07-31 21:51:28 -0300

    Criação de todos os serviços que serão utilizados no projeto marketplace.
    - cadastro
      - Crud de produtos
    - busca
      - busca por produtos
    - avaliacao
      - avalidacao do comprador referente ao produto
    - noticia
      - busca do serviço de notícias
    - processa
      - processamento dos itens enviados para a fila
    - venda
      - venda de um produto.

--- README.md
+++
@@ -1,8 +0,0 @@
-# Hotmart Challenge
-
-Esse repositório contém os desafios que são aplicados para seleção de candidatos a vagas na hotmart, ele está subdividido por área do desafios
-
-   - back-end
-   - dev-ops
-   - front-end
-   
---
+++ hotmart-challenge/busca/src/main/java/br/com/hotmart/challenge/demo/BuscaApplication.java
@@ -0,0 +1,13 @@
+package br.com.hotmart.challenge.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaApplication.class, args);
+	}
+
+}
