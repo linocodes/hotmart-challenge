a933bd8ec258151cee74a03b1564a917a3d21d4b
Marcelo Lino <marcelo.lino.santos@gmail.com>
2020-08-05 02:55:47 -0300

    Informações do commit 1 - Refatoração do servico. 2 - Ajuste no calcula de carga de notícias. 3 - Ajuste no pom.xml 4 - Criação da documentação.

--- hotmart-challenge/noticia/src/main/java/br/com/hotmart/challenge/Application.java
+++ hotmart-challenge/noticia/src/main/java/br/com/hotmart/challenge/Application.java
@@ -2,12 +2,14 @@ package br.com.hotmart.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-public class NoticiaApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NoticiaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
